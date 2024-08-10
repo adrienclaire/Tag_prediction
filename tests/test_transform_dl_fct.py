c991b05bc6d0fadcdfc7d32975f3ca81bc8b873d
adrienclaire <127939532+adrienclaire@users.noreply.github.com>
2024-08-10 17:08:27 +0000

    change and simplified test transform function testing

--- tests/test_transform_dl_fct.py
+++ tests/test_transform_dl_fct.py
@@ -13,17 +13,7 @@ def test_transform_dl_fct_basic():
     expected_output = "this is a test"
     assert transform_dl_fct(input_text) == expected_output
 
-def test_transform_dl_fct_with_mentions_and_urls():
-    input_text = "Check out this link http://example.com and mention @user"
-    expected_output = "check out this link and mention"
-    assert transform_dl_fct(input_text) == expected_output
-
-def test_transform_dl_fct_with_symbols():
-    input_text = "Test - with / symbols #!"
-    expected_output = "test - with / symbols # !"
-    assert transform_dl_fct(input_text) == expected_output
-
-def test_transform_dl_fct_with_mixed_case():
-    input_text = "This Is A Test Of Mixed CASE."
-    expected_output = "this is a test of mixed case"
+def test_transform_dl_fct_tokenization():
+    input_text = "Simple test case"
+    expected_output = "simple test case"
     assert transform_dl_fct(input_text) == expected_output
