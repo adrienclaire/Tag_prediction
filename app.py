e90097e080790dbe1b4acbb9fe5960207c67ee70
adrienclaire <127939532+adrienclaire@users.noreply.github.com>
2024-08-26 19:26:22 +0000

    comments

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import gensim
 
 app = Flask(__name__)
 
-# Use this code snippet in your app.
+# Use this code snippet in your app
 # If you need more information about configurations
 # or implementing the sample code, visit the AWS docs:
 # https://aws.amazon.com/developer/language/python/
